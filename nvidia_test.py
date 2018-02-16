3b6d0287467672fd36325f81cf9cf4f343eb32b3
Nick <nicholas.curtis@uconn.edu>
2018-02-16 15:53:53 -0500

    updates

--- nvidia_test.py
+++ nvidia_test.py
@@ -19,34 +19,24 @@ import os
 
 
 def compile_and_test(header_path, lib_path, platform_name, defines='',
-                     should_fail=False):
+                     should_fail=False, lib_name='OpenCL'):
     # define the compilation string
     compilation_template = (
-        'gcc -fPIC -O3 -std=c99 -xc {defines} jacobian_kernel_main.ocl '
+        'gcc -fPIC -O0 -g -std=c99 -xc {defines} jacobian_kernel_main.ocl '
         'jacobian_kernel_compiler.ocl timer.ocl read_initial_conditions.ocl '
-        'ocl_errorcheck.ocl -I{header_path} {lib_path} -o '
+        'ocl_errorcheck.ocl -I{header_path} -Wl,-rpath,{lib_path} -l{lib_name} -o '
         'test.out')
 
     # normalize the libpath / test for presence of the OpenCL library:
-    try:
-        lib = os.path.realpath(
-            os.path.join(os.path.realpath(lib_path), 'libOpenCL.so'))
-    except OSError:
-        raise Exception('OpenCL Library for package {} not found at:\n {}'.format(
-            platform_name, lib_path))
+    lib_path = os.path.realpath(lib_path)
+    assert os.path.isdir(lib_path), (
+        'Library directory {} for platform {} not found'.format(
+            lib_path, platform_name))
 
     # normalize the header path / test for existance
-    try:
-        header_path = os.path.realpath(header_path)
-        assert os.path.isdir(header_path)
-    except AssertionError:
-        raise Exception('OpenCL headers not found at:\n {}'.format(header_path))
-
-    # check we don't have spaces in either path
-    assert ' ' not in lib, (
-        'Currently, this tester cannot deal with spaces in library paths')
-    assert ' ' not in header_path, (
-        'Currently, this tester cannot deal with spaces in library paths')
+    header_path = os.path.realpath(header_path)
+    assert os.path.isdir(header_path), ('OpenCL headers not found at:\n {}'.format(
+        header_path))
 
     # turn defines into preprocessor macros
     if defines:
@@ -54,21 +44,30 @@ def compile_and_test(header_path, lib_path, platform_name, defines='',
 
     # transform call
     compilation_template = compilation_template.format(
-        defines=defines, header_path=header_path, lib_path=lib_path)
+        defines=defines, header_path=header_path, lib_path=lib_path,
+        lib_name=lib_name)
 
-    # now compile / run
-    with TemporaryFile() as stdout:
-        with TemporaryFile() as stderr:
-            try:
-                subprocess.check_call(compilation_template.split() + [
-                    platform_name, '896', '1', '1'], stdout=stdout, stderr=stderr)
-            except subprocess.CalledProcessError:
-                raise Exception(stderr.read())
+    def run(cmd):
+        # now compile / run
+        with TemporaryFile() as stdout:
+            with TemporaryFile() as stderr:
+                try:
+                    print(' '.join(cmd))
+                    subprocess.check_call(cmd, stdout=stdout, stderr=stderr)
+                except subprocess.CalledProcessError as e:
+                    print('Error compiling or running: {}'.format(' '.join(e.cmd)))
+                    stderr.seek(0)
+                    raise Exception(str(stderr.read()))
+            stdout.seek(0)
+            output = stdout.read()
+        return output
 
-        output = stdout.read()
+    # compile
+    run(compilation_template.split())
 
     # look for the sum the forward stoichometic coefficients - 1 of reaction 1988
     # (0-based) in the output.
+    output = run(['./test.out', platform_name, '896', '1', '1'])
 
     # The correct answer is '0' (4 * 0 + 1 - 1 = 0)
     # The incorrect answer is '-1'
@@ -109,6 +108,12 @@ if __name__ == '__main__':
                              "/opt/intel/opencl/lib64/ for Intel, or "
                              "/usr/local/lib64/ for POCL, or "
                              "/opt/AMDAPPSDK-3.0/lib/x86_64/sdk/ for AMD.")
+    parser.add_argument('-ol', '--other_opencl_libname',
+                        type=str,
+                        required=False,
+                        default='OpenCL',
+                        help='The name of the OpenCL library to link against for'
+                             'the other platform')
     parser.add_argument('-on', '--other_opencl_platform_name',
                         type=str,
                         required=True,
@@ -117,10 +122,10 @@ if __name__ == '__main__':
                              "include 'Intel' for Intel, 'AMD' for AMD, or "
                              "'Portable Computing Language' for POCL.")
 
-    args = parser.parser()
+    args = parser.parse_args()
 
     # test nvidia w/o printing -- should fail
-    compile_and_test(args.header_path, args.nvidia_path, 'NVIDIA', defines='',
+    compile_and_test(args.header_path, args.nvidia_path, 'NVIDIA',
                      should_fail=True)
 
     # finally, turn on printing for NVIDIA -- should pass
@@ -130,4 +135,4 @@ if __name__ == '__main__':
     # test other OpenCL implementation -- should pass
     compile_and_test(args.header_path, args.other_opencl_libpath,
                      args.other_opencl_platform_name, defines='',
-                     should_fail=False)
+                     should_fail=False, lib_name=args.other_opencl_libname)
